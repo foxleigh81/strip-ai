b3f598d6f00b2d93a81bf9c5589f7c463b689821
Alex Foxleigh <alex@alexfoxleigh.com>
2025-06-06 14:38:05 +0100

    Removes ref to index.css in index.html as it's already being loaded in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
-import './index.css'
+import './index.css'  
 
 // Get root element and clear loading state
 const rootElement = document.getElementById('root')!
